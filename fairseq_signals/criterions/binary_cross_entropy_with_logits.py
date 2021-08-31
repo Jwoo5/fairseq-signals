5abe234943b95f0d61add68bed77a50bbc5b410e
Jwoo5 <ojw0123@korea.ac.kr>
2021-08-31 13:02:21 +0000

    add metrics for precision , recall

--- fairseq_signals/criterions/binary_cross_entropy_with_logits.py
+++ fairseq_signals/criterions/binary_cross_entropy_with_logits.py
@@ -1,7 +1,6 @@
 import math
 from argparse import Namespace
 from dataclasses import dataclass, field
-from numpy import average
 from omegaconf import II
 from typing import Optional, List
 
@@ -15,30 +14,19 @@ from fairseq_signals import logging, metrics
 from fairseq_signals.data.ecg import ecg_utils
 from fairseq_signals.utils import utils
 from fairseq_signals.criterions import BaseCriterion, register_criterion
+from fairseq_signals.criterions.binary_cross_entropy import BinaryCrossEntropyCriterionConfig
 from fairseq_signals.dataclass import Dataclass
-# from fairseq_signals.data.data_utils import post_process
 from fairseq_signals.tasks import Task
 from fairseq_signals.logging.meters import safe_round
 
 @dataclass
-class BinaryCrossEntropyCriterionConfig(Dataclass):
-    weight: Optional[List[float]] = field(
-        default = None,
-        metadata = {
-            "help": "a manual rescaling weight given to the loss of each batch element."
-            "if given, has to be a float list of size nbatch."
-        }
-    )
+class BinaryCrossEntropyWithLogitsCriterionConfig(BinaryCrossEntropyCriterionConfig):
     pos_weight: Optional[List[float]] = field(
         default = None,
         metadata = {
             "help": "a weight of positive examples. Must be a vector with length equal to the"
             "number of classes."
         }
-    )
-    report_auc: bool = field(
-        default=False,
-        metadata={"help": "whether to report auprc / auroc metric, used for valid step"}
     )
     report_score: bool = field(
         default=False,
@@ -52,10 +40,10 @@ class BinaryCrossEntropyCriterionConfig(Dataclass):
     )
 
 @register_criterion(
-    "binary_cross_entropy_with_logits", dataclass = BinaryCrossEntropyCriterionConfig
+    "binary_cross_entropy_with_logits", dataclass = BinaryCrossEntropyWithLogitsCriterionConfig
 )
 class BinaryCrossEntropyWithLogitsCriterion(BaseCriterion):
-    def __init__(self, cfg: BinaryCrossEntropyCriterionConfig, task: Task):
+    def __init__(self, cfg: BinaryCrossEntropyWithLogitsCriterionConfig, task: Task):
         super().__init__(task)
         self.weight = cfg.weight
         self.pos_weight = cfg.pos_weight
@@ -81,6 +69,9 @@ class BinaryCrossEntropyWithLogitsCriterion(BaseCriterion):
         target = model.get_targets(sample, net_output)
 
         reduction = "none" if not reduce else "sum"
+
+        if self.pos_weight:
+            self.pos_weight = torch.tensor(self.pos_weight).to(logits.device)
 
         loss = F.binary_cross_entropy_with_logits(
             input = logits,
@@ -124,15 +115,32 @@ class BinaryCrossEntropyWithLogitsCriterion(BaseCriterion):
             if probs.numel() == 0:
                 corr = 0
                 count = 0
+                tp = 0
+                tn = 0
+                fp = 0
+                fn = 0
             else:
                 count = float(probs.numel())
                 corr = (outputs == target).sum().item()
 
+                true = torch.where(target == 1)
+                false = torch.where(target == 0)
+                tp = outputs[true].sum()
+                fn = outputs[true].numel() - tp
+                fp = outputs[false].sum()
+                tn = outputs[false].numel() - fp
+
             logging_output["correct"] = corr
             logging_output["count"] = count
 
-            labels = target.cpu().numpy()
+            logging_output["tp"] = tp.item()
+            logging_output["fp"] = fp.item()
+            logging_output["tn"] = tn.item()
+            logging_output["fn"] = fn.item()
+
             if self.report_score:
+                labels = target.cpu().numpy()
+
                 observed_score = (
                     ecg_utils.compute_scored_confusion_matrix(
                         self.score_weights,
@@ -231,6 +239,15 @@ class BinaryCrossEntropyWithLogitsCriterion(BaseCriterion):
         total = sum(log.get("count", 0) for log in logging_outputs)
         metrics.log_scalar("_total", total)
 
+        tp = sum(log.get("tp", 0) for log in logging_outputs)
+        metrics.log_scalar("_tp", tp)
+        fp = sum(log.get("fp", 0) for log in logging_outputs)
+        metrics.log_scalar("_fp", fp)
+        tn = sum(log.get("tn", 0) for log in logging_outputs)
+        metrics.log_scalar("_tn", tn)
+        fn = sum(log.get("fn", 0) for log in logging_outputs)
+        metrics.log_scalar("_fn", fn)
+
         if total > 0:
             metrics.log_derived(
                 "accuracy",
@@ -240,7 +257,25 @@ class BinaryCrossEntropyWithLogitsCriterion(BaseCriterion):
                 if meters["_total"].sum > 0
                 else float("nan")
             )
-        
+
+            metrics.log_derived(
+                "precision",
+                lambda meters: safe_round(
+                    meters["_tp"].sum / (meters["_tp"].sum + meters["_fp"].sum), 5
+                )
+                if (meters["_tp"].sum + meters["_fp"].sum) > 0
+                else float("nan")
+            )
+
+            metrics.log_derived(
+                "recall",
+                lambda meters: safe_round(
+                    meters["_tp"].sum / (meters["_tp"].sum + meters["_fn"].sum), 5
+                )
+                if (meters["_tp"].sum + meters["_fn"].sum) > 0
+                else float("nan")
+            )
+
         # builtin_keys = {
         #     "loss",
         #     "ntokens",
