1d0ebf186a4758643ecfbbf40c87d5a15f1fb425
Jwoo5 <ojw0123@korea.ac.kr>
2021-10-08 08:24:12 +0000

    move aggregation step into :func:`get_logits`

--- fairseq_signals/models/clocs/clocs.py
+++ fairseq_signals/models/clocs/clocs.py
@@ -3,11 +3,10 @@ from dataclasses import dataclass, field
 from typing import List, Tuple, Optional, Any
 from omegaconf import II
 
-import math
-
 import torch
 
-from fairseq_signals.dataclass import  ChoiceEnum
+from fairseq_signals.dataclass import ChoiceEnum
+from fairseq_signals.utils import utils
 from fairseq_signals.models import register_model
 from fairseq_signals.models.conv_transformer import ConvTransformerConfig, ConvTransformerModel
 
@@ -15,13 +14,16 @@ CLOCS_MODE_CHOICES = ChoiceEnum(["cmsc", "cmlc", "cmsmlc"])
 
 @dataclass
 class ClocsConfig(ConvTransformerConfig):
-    apply_mask: bool = True
+    apply_mask: bool = False
 
 @register_model("clocs", dataclass = ClocsConfig)
 class ClocsModel(ConvTransformerModel):
     def __init__(self, cfg: ClocsConfig):
         super().__init__(cfg)
         self.cfg = cfg
+
+        if not cfg.apply_mask:
+            self.mask_emb = None
 
     def upgrade_state_dict_named(self, state_dict, name):
         super().upgrade_state_dict_named(state_dict, name)
@@ -33,8 +35,18 @@ class ClocsModel(ConvTransformerModel):
         """Build a new model instance."""
         return cls(cfg)
     
-    def get_logits(self, net_output):
+    def get_logits(self, net_output, normalize=False, aggregate=False):
         logits = net_output["encoder_out"]
+
+        if net_output["padding_mask"] is not None and net_output["padding_mask"].any():
+            logits[net_output["padding_mask"]] = 0
+        
+        if aggregate:
+            logits = torch.div(logits.sum(dim=1), (logits != 0).sum(dim=1))
+        
+        if normalize:
+            logits = utils.log_softmax(logits.float(), dim=-1)
+
         return logits
     
     def forward(self, source, patient_id=None, segment=None, **kwargs):
