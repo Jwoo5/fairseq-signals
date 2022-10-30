b819d1b1c44552703bac239898c0d1073bd06380
Jwoo5 <ojw0123@korea.ac.kr>
2022-10-30 19:00:37 +0900

    typo

--- fairseq_signals/data/data_utils.py
+++ fairseq_signals/data/data_utils.py
@@ -30,7 +30,7 @@ def compute_mask_indices(
     Computes random mask spans for a given shape
 
     Args:
-        shape: the the shape for which to compute masks.
+        shape: the shape for which to compute masks.
             should be of size 2 where first element is batch size and 2nd is timesteps
         padding_mask: optional padding mask of the same size as shape, which will prevent masking padded elements
         mask_prob: probability for each token to be chosen as start of the span to be masked. this will be multiplied by
