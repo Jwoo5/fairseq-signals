56b549b54ad3d1255d0867d6fcbd0d2fd5d33918
Jwoo5 <ojw0123@korea.ac.kr>
2023-02-22 18:12:06 +0900

    makeup test codes

--- fairseq_cli/hydra_validate.py
+++ fairseq_cli/hydra_validate.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from fairseq_signals.dataclass.initialize import add_defaults, hydra_init
+from fairseq_signals.dataclass.utils import omegaconf_no_object_check
 from fairseq_cli.validate import main as pre_main
 from fairseq_signals import distributed_utils, metrics
 from fairseq_signals.dataclass.configs import Config
@@ -15,20 +16,30 @@ from omegaconf import OmegaConf, open_dict
 logger = logging.getLogger("fairseq_cli.hydra_validate")
 
 @hydra.main(config_path = os.path.join("..", "fairseq_signals", "config"), config_name = "config")
-def hydra_main(cfg: Config) -> None:
+def hydra_main(cfg: Config, **kwargs) -> None:
     add_defaults(cfg)
 
     if cfg.common.reset_logging:
         reset_logging() # Hydra hijacks logging, fix that
     else:
-        with open_dict(cfg):
-            # make hydra logging work with ddp (see # see https://github.com/facebookresearch/hydra/issues/1126)
-            cfg.job_logging_cfg = OmegaConf.to_container(HydraConfig.get().job_logging, resolve=True)
+        if HydraConfig.initialized():
+            with open_dict(cfg):
+                # make hydra logging work with ddp (see # see https://github.com/facebookresearch/hydra/issues/1126)
+                cfg.job_logging_cfg = OmegaConf.to_container(
+                    HydraConfig.get().job_logging, resolve=True
+                )
 
-    cfg = OmegaConf.create(OmegaConf.to_container(cfg, resolve = True, enum_to_str = True))
+    with omegaconf_no_object_check():
+        cfg = OmegaConf.create(
+            OmegaConf.to_container(cfg, resolve=True, enum_to_str=True)
+        )
     OmegaConf.set_struct(cfg, True)
 
-    distributed_utils.call_main(cfg, pre_main)
+    assert (
+        cfg.dataset.max_tokens is not None or cfg.dataset.batch_size is not None
+    ), "Must specify batch size either with --max-tokens or --batch-size"
+
+    distributed_utils.call_main(cfg, pre_main, **kwargs)
 
 def cli_main():
     try:
--- fairseq_cli/validate.py
+++ fairseq_cli/validate.py
@@ -31,18 +31,9 @@ logging.basicConfig(
 )
 logger = logging.getLogger("fairseq_cli.validate")
 
-def main(cfg: DictConfig, override_args = None):
-    if isinstance(cfg, Namespace):
-        cfg = convert_namespace_to_omegaconf(cfg)
-    
+def main(cfg: DictConfig, override_args=None):
     utils.import_user_module(cfg.common)
 
-    reset_logging()
-
-    assert (
-        cfg.dataset.max_tokens is not None or cfg.dataset.batch_size is not None
-    ), "Must specify batch size either with --max-tokens or --batch-size"
-    
     use_fp16 = cfg.common.fp16
     use_cuda = torch.cuda.is_available()
 
@@ -55,47 +46,50 @@ def main(cfg: DictConfig, override_args = None):
     else:
         data_parallel_world_size = 1
         data_parallel_rank = 0
-    
+
     if override_args is not None:
         overrides = vars(override_args)
-        overrides.update(eval(getattr(override_args, "model_overrides", "{}")))
     else:
-        overrides = None
-    
+        overrides = {}
+
+    overrides.update({"task": {"data": cfg.task.data}})
+    model_overrides = eval(getattr(cfg.common_eval, "model_overrides", "{}"))
+    overrides.update(model_overrides)
+
     # Load model
     logger.info(f"loading model from {cfg.common_eval.path}")
-    state = checkpoint_utils.load_checkpoint_to_cpu(cfg.common_eval.path, overrides)
-    model_cfg = OmegaConf.merge(state["cfg"].model, eval(cfg.common_eval.model_overrides))
+    model, saved_cfg, task = checkpoint_utils.load_model_and_task(
+        cfg.common_eval.path,
+        arg_overrides=overrides,
+        suffix=cfg.checkpoint.checkpoint_suffix
+    )
 
-    task = tasks.setup_task(cfg.task)
-    model = task.build_model(model_cfg)
+    logger.info(
+        "num. shared model params: {:,} (num. trained: {:,})".format(
+            sum(p.numel() for p in model.parameters() if not getattr(p, "expert", False)),
+            sum(p.numel() for p in model.parameters() if not getattr(p, "expert", False) and p.requires_grad)
+        )
+    )
 
-    if state is not None:
-        #XXX
-        # for legacy wav2vec model
-        # state["model"] = {k.replace('w2v_encoder.w2v_model.','encoder.'): v for k, v in state["model"].items()}
+    logger.info(
+        "num. expert model params: {} (num. trained: {})".format(
+            sum(p.numel() for p in model.parameters() if getattr(p, "expert", False)),
+            sum(p.numel() for p in model.parameters() if getattr(p, "expert", False) and p.requires_grad)
+        )
+    )
 
-        # for legacy clocs model
-        # state["model"] = {k.replace('clocs_encoder.clocs_model.encoder.w2v_model.','encoder.'): v for k, v in state["model"].items()}
-        # state["model"].pop("encoder.mask_emb")
-
-        model.load_state_dict(state["model"], strict = True)
-        logger.info(f"Loaded model checkpoint from {cfg.common_eval.path}")
-    else:
-        raise FileNotFoundError(cfg.common_eval.path)
-    
     # Move model to GPU
     model.eval()
     if use_fp16:
         model.half()
     if use_cuda:
         model.cuda()
-    
+
     # Print args
-    logger.info(pprint.pformat(dict(cfg)))
+    logger.info(pprint.pformat(dict(saved_cfg)))
 
     # Build criterion
-    criterion = task.build_criterion(cfg.criterion)
+    criterion = task.build_criterion(saved_cfg.criterion)
     criterion.eval()
 
     def _fp_convert_sample(sample):
@@ -118,32 +112,45 @@ def main(cfg: DictConfig, override_args = None):
         return sample
 
     for subset in cfg.dataset.valid_subset.split(","):
+        subset = subset.strip()
         try:
-            task.load_dataset(subset, combine = False, epoch = 1, task_cfg = cfg.task)
+            task.load_dataset(subset, combine=False, epoch=1, task_cfg=cfg.task)
             dataset = task.dataset(subset)
         except KeyError:
             raise Exception("Cannot find dataset: " + subset)
 
         # Initialize data iterator
         itr = task.get_batch_iterator(
-            dataset = dataset,
-            max_tokens = cfg.dataset.max_tokens,
-            max_signals = cfg.dataset.batch_size,
-            # max_positions = ...,
-            ignore_invalid_inputs = cfg.dataset.skip_invalid_size_inputs_valid_test,
-            required_batch_size_multiple = cfg.dataset.required_batch_size_multiple,
-            seed = cfg.common.seed,
-            num_shards = data_parallel_world_size,
-            shard_id = data_parallel_rank,
-            num_workers = cfg.dataset.num_workers,
-            data_buffer_size = cfg.dataset.data_buffer_size
-        ).next_epoch_itr(shuffle = False)
+            dataset=dataset,
+            max_tokens=cfg.dataset.max_tokens,
+            max_signals=cfg.dataset.batch_size,
+            ignore_invalid_inputs=cfg.dataset.skip_invalid_size_inputs_valid_test,
+            required_batch_size_multiple=cfg.dataset.required_batch_size_multiple,
+            seed=cfg.common.seed,
+            num_shards=data_parallel_world_size,
+            shard_id=data_parallel_rank,
+            num_workers=cfg.dataset.num_workers,
+            data_buffer_size=cfg.dataset.data_buffer_size
+        ).next_epoch_itr(shuffle=False)
         progress = progress_bar.progress_bar(
             itr,
-            log_format = cfg.common.log_format,
-            log_interval = cfg.common.log_interval,
-            prefix = f"valid on '{subset}' subset",
-            default_log_format = ("tqdm" if not cfg.common.no_progress_bar else "simple")
+            log_format=cfg.common.log_format,
+            log_interval=cfg.common.log_interval,
+            prefix=f"valid on '{subset}' subset",
+            default_log_format=("tqdm" if not cfg.common.no_progress_bar else "simple"),
+            wandb_project=(
+                cfg.common.wandb_project
+                if distributed_utils.is_master(cfg.distributed_training)
+                else None
+            ),
+            wandb_entity=(
+                cfg.common.wandb_entity
+                if distributed_utils.is_master(cfg.distributed_training)
+                else None
+            ),
+            wandb_run_name=os.environ.get(
+            "WANDB_NAME", os.path.basename(cfg.checkpoint.save_dir)
+            )
         )
 
         log_outputs = []
@@ -152,25 +159,26 @@ def main(cfg: DictConfig, override_args = None):
                 sample = utils.move_to_cuda(sample) if use_cuda else sample
                 sample = _fp_convert_sample(sample)
                 _loss, _sample_size, log_output = task.valid_step(sample, model, criterion, subset)
-                progress.log(log_output, step = i)
+                progress.log(log_output, step=i)
                 log_outputs.append(log_output)
-        
+
         if data_parallel_world_size > 1:
             log_outputs = distributed_utils.all_gather_list(
                 log_outputs,
-                max_size = cfg.common.all_gather_list_size,
-                group = distributed_utils.get_data_parallel_group()
+                max_size=cfg.common.all_gather_list_size,
+                group=distributed_utils.get_data_parallel_group()
             )
             log_outputs = list(chain.from_iterable(log_outputs))
         
         with metrics.aggregate() as agg:
             task.reduce_metrics(log_outputs, criterion)
             log_output = agg.get_smoothed_values()
-        
-        progress.print(log_output, tag = subset, step = i)
 
         if hasattr(task, "post_validate"):
             task.post_validate(model, log_output, agg, num_updates=0)
+        
+        progress.print(log_output, tag=subset, step=i)
+
 
 def cli_main():
     parser = options.get_validation_parser()
@@ -183,7 +191,7 @@ def cli_main():
     )
 
     distributed_utils.call_main(
-        convert_namespace_to_omegaconf(args), main, override_args = override_args
+        convert_namespace_to_omegaconf(args), main, override_args=override_args
     )
 
 if __name__ == "__main__":
